04641adf528b454df3c912c64688b2109bae88f8
Cristian Aguilar <cristian.aguilar.iei@gmail.com>
2026-03-30 16:46:56 -0300

    update app listen port

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,8 @@ async function bootstrap() {
     })
   );
 
-  await app.listen(process.env.PORT ?? 4000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
   console.log(`App running on port: ${process.env.PORT}`)
 }
 bootstrap();
